a939817312d937dd7dee823801988d94a906a316
RitwikGupta <ritwikg2004@live.com>
2013-07-07 17:00:18 -0400

    Uses palindromesTwo.tsv

--- palindromes.py
+++ palindromes.py
@@ -7,10 +7,7 @@ Created on Sun Jul  7 12:47:51 2013
 
 arr = []
 
-for line in open("palindromes.tsv"):
-    tab = line.index("\t")
-    start = line[:tab]
-    sequence = line[tab + 1: len(line) - 1]
+for line in open("palindromesTwo.tsv"):
     
     for i in range(0, len(line) - 4):
         temp = line[i:i + 4]
