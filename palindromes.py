0c0aa4cc72764b5a76a6da649455cbf8ba32ec4d
RitwikGupta <ritwikg2004@live.com>
2013-07-07 13:48:32 -0400

    prints out array of palindromes. TODO: fix numbers

--- palindromes.py
+++ palindromes.py
@@ -5,8 +5,17 @@ Created on Sun Jul  7 12:47:51 2013
 @author: Ritwik
 """
 
+arr = []
+
 for line in open("palindromes.tsv"):
     tab = line.index("\t")
     start = line[:tab]
     sequence = line[tab + 1: len(line) - 1]
-    print(sequence)
+    
+    for i in range(0, len(line) - 4):
+        temp = line[i:i+4]
+        reverse = temp[::-1]
+        if temp == reverse:
+            arr.append(temp)
+            
+print(arr)
