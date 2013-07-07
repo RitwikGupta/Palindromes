3c1748d5d67cec96a3db42e53fab0eefdd7300d6
RitwikGupta <ritwikg2004@live.com>
2013-07-07 17:43:12 -0400

    removed column heading

--- Java/src/Palindromes.java
+++ Java/src/Palindromes.java
@@ -4,7 +4,7 @@ public class Palindromes {
 	
 	public static void main(String[] args) throws IOException{
 		
-		BufferedReader reader = new BufferedReader(new FileReader("./palindromesTWO.tsv"));
+		BufferedReader reader = new BufferedReader(new FileReader("../palindromesTWO.tsv"));
 		String line = null;
 		
 		while((line = reader.readLine()) != null) {
