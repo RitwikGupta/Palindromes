65ca8f4b64766f6b69b7dd300721699900b76c06
RitwikGupta <ritwikg2004@live.com>
2013-07-07 17:40:55 -0400

    added java

---
+++ Java/src/Palindromes.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Palindromes {
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader reader = new BufferedReader(new FileReader("./palindromesTWO.tsv"));
+		String line = null;
+		
+		while((line = reader.readLine()) != null) {
+			
+			System.out.println(line);
+			
+		}
+				
+	}
+
+}
