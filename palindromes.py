e553cf03b1e045fd3b10e185bceea17a1dbcb8c2
RitwikGupta <ritwikg2004@live.com>
2013-07-07 17:16:26 -0400

    write palindromes to file

--- palindromes.py
+++ palindromes.py
@@ -7,12 +7,15 @@ Created on Sun Jul  7 12:47:51 2013
 
 arr = []
 
-for line in open("palindromesTwo.tsv"):
+with open("palindromesOUT.txt", "wt") as out:
     
-    for i in range(0, len(line) - 4):
-        temp = line[i:i + 4]
-        reverse = temp[::-1]   #extended slice syntax.
-        if temp == reverse:
-            arr.append(temp)
+    for line in open("palindromesTwo.tsv"):
+        
+        for i in range(0, len(line) - 4):
+            temp = line[i:i + 4]
+            reverse = temp[::-1]   #extended slice syntax.
+            if temp == reverse:
+                arr.append(temp)
+                out.write(temp + "\n")
             
 print(arr)
