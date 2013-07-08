2a02fcf7c03427173e4eb0e65a7284522ed703a5
RitwikGupta <ritwikg2004@live.com>
2013-07-08 14:30:36 -0400

    any window size

--- palindromes.py
+++ palindromes.py
@@ -8,6 +8,9 @@ Created on Sun Jul  7 12:47:51 2013
 dic = {}
 countArr = [0,0,0,0,0]
 
+window = int(input("Window: "))
+number = int(input("Number: "))
+arr = [i for i in range(number + 1)]
     
 for line in open("palindromesTHREE.tsv"):
     
@@ -16,17 +19,10 @@ for line in open("palindromesTHREE.tsv"):
     value = line[tab + 1:len(line) - 1]
     
     dic[key] = value
-        
+
 for key in dic:
-    if int(key) >= 0 and int(key) < 10000000:
-        countArr[0] += 1
-    elif int(key) >= 10000000 and int(key) < 20000000:
-        countArr[1] += 1
-    elif int(key) >= 20000000 and int(key) < 30000000:
-        countArr[2] += 1
-    elif int(key) >= 30000000 and int(key) < 40000000:
-        countArr[3] += 1
-    else:
-        countArr[4] += 1
-    
+    for i in range(number + 1):
+        if int(key) >= i*window and int(key) < (i+1)*window:
+            countArr[i] += 1
+            
 print(countArr)
