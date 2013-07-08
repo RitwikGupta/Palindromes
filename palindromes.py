9a3cfb7617edbb962a57913d7cbf611cacbfc0da
RitwikGupta <ritwikg2004@live.com>
2013-07-08 14:49:18 -0400

    added comments

--- palindromes.py
+++ palindromes.py
@@ -9,18 +9,24 @@ dic = {}
 
 window = int(input("Window: "))
 number = int(input("Number: "))
+
+#initialize array with 'number' + 1 elements of 0
 arr = [0 for i in range(number + 1)]
     
 for line in open("palindromesTHREE.tsv"):
     
     tab = line.index("\t")
     key = line[:tab]
+    
+    #line - 1 because there is a /n at the end
     value = line[tab + 1:len(line) - 1]
     
     dic[key] = value
 
 for key in dic:
     for i in range(number + 1):
+        
+        #multiples of window
         if int(key) >= i*window and int(key) < (i+1)*window:
             arr[i] += 1
     
