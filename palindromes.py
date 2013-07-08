859c3e4e574b58e29228160c42aea989faa739f3
RitwikGupta <ritwikg2004@live.com>
2013-07-08 14:38:42 -0400

    fixed window size bug

--- palindromes.py
+++ palindromes.py
@@ -6,11 +6,10 @@ Created on Sun Jul  7 12:47:51 2013
 """
 
 dic = {}
-countArr = [0,0,0,0,0]
 
 window = int(input("Window: "))
 number = int(input("Number: "))
-arr = [i for i in range(number + 1)]
+arr = [0 for i in range(number + 1)]
     
 for line in open("palindromesTHREE.tsv"):
     
@@ -23,6 +22,6 @@ for line in open("palindromesTHREE.tsv"):
 for key in dic:
     for i in range(number + 1):
         if int(key) >= i*window and int(key) < (i+1)*window:
-            countArr[i] += 1
-            
-print(countArr)
+            arr[i] += 1
+    
+print(arr)
