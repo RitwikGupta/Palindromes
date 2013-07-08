d111dc7b8e2d237f0ef9999331913cc3b6e3084a
RitwikGupta <ritwikg2004@live.com>
2013-07-07 22:47:16 -0400

    Revert "Add"
    This reverts commit 92e623ae6ac82f12e569fa21c9ac3b1db6ca60b0.

--- palindromes.py
+++ palindromes.py
@@ -5,28 +5,20 @@ Created on Sun Jul  7 12:47:51 2013
 @author: Ritwik
 """
 
-dic = {}
-countOne = 0; countTwo = 0; countThr = 0; countFou = 0;
+arr = []
+counter = 0
 
 with open("palindromesOUT.txt", "wt") as out:
     
-    for line in open("palindromesTHREE.tsv"):
+    for line in open("palindromesTwo.tsv"):
         
-        tab = line.index("\t")
-        key = line[:tab]
-        value = line[tab + 1:len(line) - 1]
-        
-        dic[key] = value
-        
-for key in dic:
-    if int(key[0]) == 1:
-        countOne += 1
-    elif int(key[0]) == 2:
-        countTwo += 1
-    elif int(key[0]) == 3:
-        countThr += 1
-    elif int(key[0]) == 4:
-        countFou += 1
-        
-print("countOne: " + str(countOne), "\ncountTwo: " + str(countTwo))
-print("countThr: " + str(countThr), "\ncountFou: " + str(countFou))
+        for i in range(0, len(line) - 4):
+            temp = line[i:i + 4]
+            reverse = temp[::-1]   #extended slice syntax.
+            if temp == reverse:
+                arr.append(temp)
+                counter += 1
+                out.write(temp + "\n")
+            
+print(arr)
+print("\n" + str(counter) + " palindromes in given file")
