4b2b8f6933d122cd5bb030cbb352413a142742de
RitwikGupta <ritwikg2004@live.com>
2013-07-08 21:39:05 -0400

    fixed window length

--- palindromes.py
+++ palindromes.py
@@ -6,27 +6,30 @@ Created on Sun Jul  7 12:47:51 2013
 """
 
 dic = {}
-
+arrTwo = []
 window = int(input("Window: "))
 
 #initialize array with 'number' + 1 elements of 0
     
-for line in open("palins/chr1-fa-palin-uniq-txt.tsv"):
+for line in open("./palins/chr1-fa-palin-uniq-txt.tsv"):
     
     tab = line.index("\t")
     key = line[:tab]
     
     #line - 1 because there is a /n at the end
-    value = line[tab + 1:len(line) - 1]
+    value = line[tab + 1:len(line)]
     
     dic[key] = value
+    arrTwo.append(key)
+    
+print(int(arrTwo[len(arrTwo) - 1]))
+number = int(arrTwo[len(arrTwo) - 1]) / window
 
-number = int(int(max(k for k, v in dic.items() if v != 0)) / window)
-arr = [0 for i in range(number + 1)]
+arr = [0 for i in range(0, int(number + 1))]
 
 for key in dic:
-    for i in range(number + 1):
-
+    
+    for i in range(int(number + 1)):
         #multiples of window
         if int(key) >= i*window and int(key) < (i+1)*window:
             arr[i] += 1
