a2d851b85f9c1dcbceae391043bb64d14a62dc2e
RitwikGupta <ritwikg2004@live.com>
2013-07-10 19:56:55 -0400

    added intron count

--- exons.py
+++ exons.py
@@ -4,11 +4,12 @@ Created on Tue Jul  9 12:31:11 2013
 
 @author: Ritwik
 """
-def exonFind(chrom):
-    temp, exonStart, exonEnd = [],[],[]
+
+def pdromeCount(chrom):
+    temp, exonStart, exonEnd, intronStart, intronEnd = [],[],[],[],[]
     dic = {}
     arrKey = []
-    exonScore = 0
+    exonScore, intronScore = 0,0
     #chrom = input("Chromosome number: ")
     
     with open("./palins/chr" + chrom + "-fa-palin-uniq-txt.tsv") as f:
@@ -21,6 +22,7 @@ def exonFind(chrom):
             dic[key] = value
             
             arrKey.append(float(key))
+            setKey = set(arrKey)
     
     with open("genes_RefSeq_KnownGene.txt") as f:
         next(f)
@@ -30,16 +32,26 @@ def exonFind(chrom):
                 exonStart += map(int, exonStartWork)
                 exonEndWork = line.split("\t")[9].strip(",").split(",")
                 exonEnd += map(int, exonEndWork)
+    
+    for i in range(len(exonStart) - 1):
+        intronStart.append(exonEnd[i])
+        intronEnd.append(exonStart[i + 1])
+    
     for i in range(len(exonStart)):
-        for key in arrKey:
+        for key in setKey:
             if key >= exonStart[i] and key <= exonEnd[i]:
                 exonScore += 1
+    for i in range(len(intronStart)):
+        for key in setKey:
+            if key >= intronStart[i] and key <= intronEnd[i]:
+                intronScore += 1
                     
     print("The exon palindrome count for chromosome " + chrom + " is %s" % exonScore)
+    print("The intron palindrome count for chromosome " + chrom + " is %s\n" % intronScore)
     
 
 for i in range(1,9):
-    exonFind(str(i))
-exonFind("10")
+    pdromeCount(str(i))
+pdromeCount("10")
 for i in range(12,23):
-    exonFind(str(i))
+    pdromeCount(str(i))
