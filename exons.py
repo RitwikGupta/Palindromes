2e1618cc6d50a7e8eab7e665b27291ba83f10060
RitwikGupta <ritwikg2004@live.com>
2013-07-10 07:54:00 -0400

    Run exon for all chromosomes. 14 throws an error

--- exons.py
+++ exons.py
@@ -4,37 +4,47 @@ Created on Tue Jul  9 12:31:11 2013
 
 @author: Ritwik
 """
-
-temp, exonStart, exonEnd = [],[],[]
-dic = {}
-arrKey = []
-exonScore = 0
-
-chrom = input("Chromosome number: ")
-
-with open("./palins/chr" + chrom + "-fa-palin-uniq-txt.tsv") as f:
-    next(f)
-    for line in f:
+def exonFind(chrom):
+    temp, exonStart, exonEnd = [],[],[]
+    dic = {}
+    arrKey = []
+    exonScore = 0
     
-        tab = line.index("\t")
-        key = line[:tab]
-        value = line[tab + 1:len(line) - 1]        
-        dic[key] = value
+    #chrom = input("Chromosome number: ")
+    
+    with open("./palins/chr" + chrom + "-fa-palin-uniq-txt.tsv") as f:
+        next(f)
+        for line in f:
         
-        arrKey.append(float(key))
-
-with open("genes_RefSeq_KnownGene.txt") as f:
-    next(f)
-    for line in f:
-        if(line.split("\t")[1] == ("chr" + chrom)):
-            exonStartWork = line.split("\t")[8].strip(",").split(",")
-            exonStart += map(float, exonStartWork)
-            exonEndWork = line.split("\t")[9].strip(",").split(",")
-            exonEnd += map(float, exonEndWork)
+            tab = line.index("\t")
+            key = line[:tab]
+            value = line[tab + 1:len(line) - 1]        
+            dic[key] = value
             
-for i in range(len(exonStart)):
-    for key in arrKey:
-        if key >= exonStart[i] and key <= exonEnd[i]:
-            exonScore += 1
+            arrKey.append(float(key))
+    
+    with open("genes_RefSeq_KnownGene.txt") as f:
+        next(f)
+        for line in f:
+            if(line.split("\t")[1] == ("chr" + chrom)):
+                exonStartWork = line.split("\t")[8].strip(",").split(",")
+                exonStart += map(float, exonStartWork)
+                exonEndWork = line.split("\t")[9].strip(",").split(",")
+                exonEnd += map(float, exonEndWork)
                 
-print("The exon palindrome count is %s" % exonScore)
+    for i in range(len(exonStart)):
+        for key in arrKey:
+            if key >= exonStart[i] and key <= exonEnd[i]:
+                exonScore += 1
+                    
+    print("The exon palindrome count for chromosome " + chrom + " is %s" % exonScore)
+    """WE HAVE PALINDROMES > 10 BASES. THEIR MINIMUM WAS 6"""
+
+
+for i in range(1,9):
+    exonFind(str(i))
+exonFind(str(10))
+for i in range(12,23):
+    exonFind(str(i))
+    """14 THROWS AN ERROR"""    
+    
