ccb9fb8e1b3bf0181f80a4734286225b0925f14d
RitwikGupta <ritwikg2004@live.com>
2013-07-12 19:17:44 -0400

    Immensely improved run time. Have to fix introns

--- exonintroncount.py
+++ exonintroncount.py
@@ -8,67 +8,113 @@ import time
 
 def pdromeCount(chrom):
     temp, exonStart, exonEnd, intronStart, intronEnd = [],[],[],[],[]
-    dic = {}
+    exonSet, intronSet = set(),set()
     arrKey = []
     exonScore, intronScore, t0, tf, sl = 0,0,0,0,0
-    
+
+#==============================================================================
+#     """------------Module 1------------"""
+#==============================================================================
+    t0 = time.clock()
     with open("./myuniq/chr" + chrom + ".fa.palin.myuniq.txt") as f:
         next(f)
         for line in f:
-        
+
             tab = line.index("\t")
             tab2 = line.index("\t", tab+1)
             key = line[:tab]
-            value = line[tab2 + 1:len(line) - 1]        
-            dic[key] = value
-            
+            value = line[tab2 + 1:len(line) - 1]
+
             arrKey.append(float(key))
-            sl += len(value)            
-            
+            sl += len(value)
+    tf = time.clock()
+    print("Elapsed time to read chromosome file: " + str(tf-t0))
+#==============================================================================
+#     """------------Module 1------------"""
+#==============================================================================
+
+#==============================================================================
+#     """------------Module 2------------"""
+#==============================================================================
     with open("genes_RefSeq_KnownGene.txt") as f:
+        #skip header line
         next(f)
         t0 = time.clock()
         for line in f:
             if(line.split("\t")[1] == ("chr" + chrom)):
+
+                #gets the exon start group in that line
                 exonStartWork = line.split("\t")[8].strip(",").split(",")
                 exonStart += map(int, exonStartWork)
+                #gets the exon end group in that line
                 exonEndWork = line.split("\t")[9].strip(",").split(",")
                 exonEnd += map(int, exonEndWork)
+
         tf = time.clock()
         print("Elapsed time to split and map exons from gene file: " + str(tf-t0))
-    
+#==============================================================================
+#         """------------Module 2------------"""
+#==============================================================================
+
+#==============================================================================
+#     """------------Module 3------------"""
+#==============================================================================
     t0 = time.clock()
+    #adds the spaces left between the exons as introns
     for i in range(len(exonStart) - 1):
+
         intronStart.append(exonEnd[i])
         intronEnd.append(exonStart[i + 1])
+
     tf = time.clock()
     print("Elapsed time to calculate and add introns to array: " + str(tf-t0))
-        
-    """------------The search starts here------------"""
+#==============================================================================
+#     """------------Module 4------------"""
+#==============================================================================
+
+#==============================================================================
+#     """------------The search starts here / Module 5------------"""
+#==============================================================================
     t0 = time.clock()
+
     for i in range(len(exonStart)):
-        for key in arrKey:
-            if key >= exonStart[i] and key <= exonEnd[i]:
-                exonScore += 1
-                """if exonScore % 10 == 0:
-                    #prints the exonScore at intervals of 10
-                    print(exonScore)"""
+
+        #makes a list of all the positions in an exon
+        fooList = [i for i in range(exonStart[i], exonEnd[i] + 1)]
+        #appends the list to the exon set
+        exonSet.update(set(fooList))
+
+    for key in arrKey:
+
+        if key in exonSet:
+            exonScore += 1
+
     tf = time.clock()
     print("Elapsed time to search exons: " + str(tf-t0))
-    
+
     t0= time.clock()
-    for i in range(len(intronStart)):
+    for k in range(1, 25):
+        for i in range(int(k*(len(intronStart) / 25)), int(k+1*(len(intronStart) / 25))):
+
+            #makes a list of the positions in an intron
+            fooList = [i for i in range(intronStart[i], intronEnd[i] + 1)]
+            #appends the list to the intron set
+            intronSet.update(set(fooList))
+
         for key in arrKey:
-            if key >= intronStart[i] and key <= intronEnd[i]:
+            if key in intronSet:
                 intronScore += 1
-    tf = time.clock()
-    print("Elapsed time to search introns: " + str(tf-t0))    
-    """------------The search ends here------------"""
-                    
+
+        tf = time.clock()
+    print("Elapsed time to search introns: " + str(tf-t0))
+#==============================================================================
+#     """------------The search ends here / Module 5------------"""
+#==============================================================================
+
     print("The exon palindrome count for chromosome " + chrom + " is %s" % exonScore)
     print("The intron palindrome count for chromosome " + chrom + " is %s" % intronScore)
-    print("The score length for chromosome " + chrom + " is %s\n" % (sl/8))
-    
+    print("The score length for chromosome " + chrom + " is %s\n" % (sl/8.0))
+
 #the function needs the number of chromosome as a string
 for i in range(2,23):
     pdromeCount(str(i))
