42d25aa0cf5ae642c52d31659d033e1dbadb7e7d
RitwikGupta <ritwikg2004@live.com>
2013-08-02 09:41:29 -0400

    Lots of file additions and code fixes

--- allrecordsfileoutput.py
+++ allrecordsfileoutput.py
@@ -32,8 +32,8 @@ def pdromeCount(chrom):
     tf = time.clock()
     print("Elapsed time to read chromosome file: " + str(tf-t0))
 
-    with open("hgTablesOut.txt", "a") as out:        
-        with open("hgTables.txt") as f:
+    with open("AllIntergenicOut.txt", "a") as out:        
+        with open("AllIntergenic.txt") as f:
             
             #skip header line
             next(f)
@@ -41,14 +41,14 @@ def pdromeCount(chrom):
             t0 = time.clock()
             for line in f:
 
-                if(line.split("\t")[1] == ("chr" + chrom)):
+                if(line.split("\t")[0] == ("chr" + chrom)):
                     
                     #gets the exon start group in that line
-                    exonStartWork = line.split("\t")[8].strip(",").split(",")
+                    exonStartWork = line.split("\t")[1].strip(",").split(",")
                     exonStart += map(int, exonStartWork)
                     
                     #gets the exon end group in that line
-                    exonEndWork = line.split("\t")[9].strip(",").split(",")
+                    exonEndWork = line.split("\t")[2].strip(",").split(",")
                     exonEnd += map(int, exonEndWork)
                     
             tf = time.clock()
--- exonintroncount.py
+++ exonintroncount.py
@@ -25,6 +25,8 @@ Created on Tue Jul  9 12:31:11 2013
 "   CoSBBI - DBMI Pitt                            "
 "                                                 "
 """""""""""""""""""""""""""""""""""""""""""""""""""
+import os
+
 def pdromeCount(chrom):
     
     exonStart, exonEnd, intronStart, intronEnd, arrKey = [],[],[],[],[]
@@ -41,66 +43,74 @@ def pdromeCount(chrom):
             tab = line.index("\t")
             tab2 = line.index("\t", tab+1)
             key = line[:tab]
-            value = line[tab2 + 1:len(line) - 1]
+            value = line[tab2 + 1:len(line) - 1].strip()
             
             dic[key] = value            
 
             arrKey.append(float(key))
-    
-    with open("hgTablesOut.txt") as f:
-        
-        #skip header line
-        next(f)
-              
-        for line in f:
-            if(line.split("\t")[1] == ("chr" + chrom)):
 
-                #gets the exon start group in that line
-                exonStartWork = line.split("\t")[8].strip(",").split(",")
-                exonStart += map(int, exonStartWork)
-                #gets the exon end group in that line
-                exonEndWork = line.split("\t")[9].strip(",").split(",")
-                exonEnd += map(int, exonEndWork)
-        print(len(exonStart))
+    with open("ScSlOut.txt", "a") as out:
+        with open("hgTablesOut.txt") as f:
+            
+            #skip header line
+            next(f)
+                  
+            for line in f:
+                if(line.split("\t")[0] == ("chr" + chrom)):
 
-    #adds intron Starts and Ends to individual lists
-    for i in range(len(exonStart) - 1):
+                    #gets the exon start group in that line
+                    exonStartWork = line.split("\t")[1].strip(",").split(",")
+                    exonStart += map(int, exonStartWork)
+                    #gets the exon end group in that line
+                    exonEndWork = line.split("\t")[2].strip(",").split(",")
+                    exonEnd += map(int, exonEndWork)
+            print(len(exonStart))
 
-        intronStart.append(exonEnd[i])
-        intronEnd.append(exonStart[i + 1])
+        #adds intron Starts and Ends to individual lists
+        for i in range(len(exonStart) - 1):
 
-    #Lookup key in exonSet
+            intronStart.append(exonEnd[i])
+            intronEnd.append(exonStart[i + 1])
 
-    for i in range(len(exonStart)):
+        #Lookup key in exonSet
 
-        #makes a list of all the positions in an exon
-        fooList = [i for i in range(exonStart[i], exonEnd[i] + 1)]
-        #appends the list to the exon set
-        exonSet.update(set(fooList))
+        for i in range(len(exonStart)):
 
-    for key in arrKey:
+            #makes a list of all the positions in an exon
+            fooList = [i for i in range(exonStart[i], exonEnd[i] + 1)]
+            #appends the list to the exon set
+            exonSet.update(set(fooList))
 
-        if key in exonSet:
-            exonScore += 1
-            slExon += len(dic[str(int(key))])
+        for i in range(len(intronStart)):
 
-    #Lookup key in intronSet
+            #makes a list of all the position in an intron
+            fooLost = [i for i in range(intronStart[i], intronEnd[i] + 1)]
+            #appends the list to the intron set
+            intronSet.update(set(fooLost))
 
-    for key in arrKey:
+        for key in arrKey:
 
-        if key not in exonSet:
-            intronScore += 1
-            slIntron += len(dic[str(int(key))])
+            if key in exonSet:
+                exonScore += 1
+                slExon += len(dic[str(int(key))])
 
-    print("The exon palindrome count for chromosome " + chrom + " is %s" % exonScore)
-    print("The intron palindrome count for chromosome " + chrom + " is %s" % intronScore)
-    print("The score length for exons of chromosome " + chrom + " is %s" % (slExon/8.0))
-    print("The score length for introns of chromosome " + chrom + " is %s\n" % (slIntron/8.0)) #new line at end
+        #Lookup key in intronSet
+
+        for key in arrKey:
+
+            if key in intronSet:
+                intronScore += 1
+                slIntron += len(dic[str(int(key))])
+
+        out.write("The exon palindrome count for chromosome " + chrom + " is %s" % exonScore)
+        out.write("The intron palindrome count for chromosome " + chrom + " is %s" % intronScore)
+        out.write("The score length for exons of chromosome " + chrom + " is %s" % (slExon/8.0))
+        out.write("The score length for introns of chromosome " + chrom + " is %s\n" % (slIntron/8.0)) #new line at end
 
 #---------------MAIN---------------
     
-"""for i in range(1,23):
+for i in range(1,23):
     pdromeCount(str(i))
 pdromeCount("X")
-pdromeCount("Y")"""
+pdromeCount("Y")
 pdromeCount("21")
